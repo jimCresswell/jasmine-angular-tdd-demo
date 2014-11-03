710a1ee100bb5a205a76648f3e0defca066f5c32
jimCresswell <jim.cresswell@gmail.com>
2014-11-03 15:14:44 +0000

    Added a nested describe and negated test condition

--- spec/expensesDataServiceSpec.js
+++ spec/expensesDataServiceSpec.js
@@ -2,10 +2,6 @@ describe('expensesDataService', function() {
 
     beforeEach(module('app'));
 
-    beforeEach(function() {
-        jasmine.addMatchers(customMatchers);
-    });
-
     it('Should return three expense items', inject(function(expensesDataService) {
         expect(expensesDataService.getExpenses().length).toBe(3);
     }));
@@ -18,8 +14,27 @@ describe('expensesDataService', function() {
        expect(expenseItems).toContain(testExpenseItem);
     }));
 
-    it('taxi should be a reasonable price', function() {
-        var taxi = new ExpenseItem('Taxi', 'blah', 89.95);
-        expect(taxi).toBeAReasonableExpense();
+    describe('reasonable expenses', function() {
+        var taxi;
+        var dinner;
+
+        beforeEach(function() {
+            jasmine.addMatchers(customMatchers);
+        });
+
+        beforeEach(function() {
+            taxi = new ExpenseItem('Taxi', 'blah', 89.95);
+            dinner = new ExpenseItem('dinner', 'dinnnner', 189.50);
+        });
+
+        it('taxi should be a reasonable price', function() {
+            expect(taxi).toBeAReasonableExpense();
+        })
+
+        it('dinner should not be a reasonable price', function() {
+            expect(dinner).not.toBeAReasonableExpense();
+        })
     })
+
+
 });
