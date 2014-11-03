bff5483eb85a41f5be216f6d7a0722f321563a98
jimCresswell <jim.cresswell@gmail.com>
2014-11-03 09:46:07 +0000

    First passing test

--- spec/ExpenseSpec.js
+++ spec/ExpenseSpec.js
@@ -1,5 +1,7 @@
 describe("Expense objects", function() {
-	it("Should be of type ExpenseItem", function() {
-
-	});
+    it("Should be of type ExpenseItem", function() {
+        var expenseItem = new ExpenseItem();
+        var expense = new Expense(expenseItem);
+        expect(expense.expenseItem).toBe(expenseItem);
+    });
 });
