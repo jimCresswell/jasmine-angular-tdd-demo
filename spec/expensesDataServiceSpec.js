337477f5e9a4ff1010015363ca3e030229b28f46
jimCresswell <jim.cresswell@gmail.com>
2014-11-03 14:32:42 +0000

    Adding a test

--- spec/expensesDataServiceSpec.js
+++ spec/expensesDataServiceSpec.js
@@ -6,4 +6,12 @@ describe('expensesDataService', function() {
         expect(expensesDataService.getExpenses().length).toBe(3);
     }));
 
+    it('Should return a taxi expense', inject(function(expensesDataService) {
+       var expenseItems = expensesDataService.getExpenses();
+
+       // Argh, magic values... Mocks to come later.
+       var testExpenseItem = new ExpenseItem('Taxi', 'blah', 89.95);
+       expect(expenseItems).toContain(testExpenseItem);
+    }));
+
 });
